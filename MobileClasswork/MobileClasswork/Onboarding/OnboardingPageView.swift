d478c0f34c6bbb6522ada41656ff78594ab4a565
Samuel Folledo <samuelfolledo@gmail.com>
2020-01-30 11:13:59 -0800

    [add] uiview subclass

---
+++ MobileClasswork/MobileClasswork/Onboarding/OnboardingPageView.swift
@@ -0,0 +1,21 @@
+//
+//  OnboardingPageView.swift
+//  MobileClasswork
+//
+//  Created by Macbook Pro 15 on 1/30/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class OnboardingPageView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
