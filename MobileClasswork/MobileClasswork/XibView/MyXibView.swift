eb82069b851b811096bb06a1c9feb7d2ccce74e5
Samuel Folledo <samuelfolledo@gmail.com>
2020-01-30 11:28:12 -0800

    [add] MyXibView

---
+++ MobileClasswork/MobileClasswork/XibView/MyXibView.swift
@@ -0,0 +1,21 @@
+//
+//  MyXibView.swift
+//  MobileClasswork
+//
+//  Created by Macbook Pro 15 on 1/30/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class MyXibView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
