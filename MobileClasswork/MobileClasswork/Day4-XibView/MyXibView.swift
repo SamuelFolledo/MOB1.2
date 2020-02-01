203b3ca70e8accf212c41fffec770f8d0d273541
Samuel Folledo <samuelfolledo@gmail.com>
2020-01-31 23:27:57 -0800

    [add] connected outlets and commonInit method

--- MobileClasswork/MobileClasswork/Day4-XibView/MyXibView.swift
+++ MobileClasswork/MobileClasswork/Day4-XibView/MyXibView.swift
@@ -9,13 +9,27 @@
 import UIKit
 
 class MyXibView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    @IBOutlet var contentView: UIView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var descLabel: UILabel!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
     }
-    */
-
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    fileprivate func commonInit() {
+        Bundle.main.loadNibNamed("MyXibView", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.frame = self.bounds
+        contentView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+    }
 }
