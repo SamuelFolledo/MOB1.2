a05a3767532cb81410b5f70c07faad43f8a5b4d0
Samuel Folledo <samuelfolledo@gmail.com>
2020-02-01 00:27:01 -0800

    [add] some xib cells

---
+++ MobileClasswork/MobileClasswork/Xib-Cell/CollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionViewCell.swift
+//  MobileClasswork
+//
+//  Created by Macbook Pro 15 on 1/31/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
