95108e09710bcf5d90acd2419513be97ca1bb335
Samuel Folledo <samuelfolledo@gmail.com>
2020-02-02 11:46:25 -0800

    [add] lineChartView and add constraints and connection outlets [add] populate the chart 🔥

--- MobileClasswork/MobileClasswork/Playground/ChartsVC.swift
+++ MobileClasswork/MobileClasswork/Playground/ChartsVC.swift
@@ -7,24 +7,65 @@
 //
 
 import UIKit
+import Charts
 
 class ChartsVC: UIViewController {
-
+    
+//MARK: Properties
+    var dataEntries: [ChartDataEntry] = []
+    
+//MARK: IBOutlets
+    @IBOutlet weak var chartView: LineChartView!
+    
+//MARK: AppLifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupLineChart(values: [24.0, 39, 0.8, -10, 50, 64, 32, 43])
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    fileprivate func setupLineChart(values: [Double]) {
+        chartView.noDataText = "No Data available"
+        for i in 0..<values.count {
+            print("chart point : \(values[i])")
+            let dataEntry = ChartDataEntry(x: Double(i), y: values[i])
+            dataEntries.append(dataEntry)
+        }
+        let line1 = LineChartDataSet(entries: dataEntries, label: "Units consumed")
+        line1.colors = [.blue]
+        line1.mode = .linear //cubic, horizontal, linear, stepped
+        line1.cubicIntensity = 0.2
+        
+        let gradient = getGradientFilling()
+        line1.fill = Fill.fillWithLinearGradient(gradient, angle: 90.0)
+        line1.drawFilledEnabled = true
+        
+        let data = LineChartData()
+        data.addDataSet(line1)
+        chartView.data = data
+        chartView.setScaleEnabled(false)
+        chartView.animate(xAxisDuration: 1.5)
+        chartView.drawGridBackgroundEnabled = false
+        chartView.xAxis.drawAxisLineEnabled = false
+        chartView.xAxis.drawGridLinesEnabled = false
+        chartView.leftAxis.drawAxisLineEnabled = false
+        chartView.leftAxis.drawGridLinesEnabled = false
+        chartView.rightAxis.drawAxisLineEnabled = false
+        chartView.rightAxis.drawGridLinesEnabled = false
+        chartView.legend.enabled = false
+        chartView.xAxis.enabled = false
+        chartView.leftAxis.enabled = false
+        chartView.rightAxis.enabled = false
+        chartView.xAxis.drawLabelsEnabled = false
+        
     }
-    */
-
+    
+    /// Gradient for filling space under the line chart
+    func getGradientFilling() -> CGGradient {
+        let topColor = UIColor(red: 141/255, green: 133/255, blue: 22/255, alpha: 1).cgColor
+        let btmColor = UIColor(red: 230/255, green: 155/255, blue: 210/255, alpha: 1).cgColor
+        let gradientColors = [topColor, btmColor] as CFArray
+        let colorLocations: [CGFloat] = [0.7,0.5]
+        return CGGradient(colorsSpace: CGColorSpaceCreateDeviceRGB(), colors: gradientColors, locations: colorLocations)!
+    }
+    
 }
