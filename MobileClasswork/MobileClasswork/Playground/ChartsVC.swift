7c0f5ec4d6e2847e7b282cb8926b20418fcef199
Samuel Folledo <samuelfolledo@gmail.com>
2020-02-02 18:41:56 -0800

    [add] implemented Charts API but xAxis still not as I want

--- MobileClasswork/MobileClasswork/Playground/ChartsVC.swift
+++ MobileClasswork/MobileClasswork/Playground/ChartsVC.swift
@@ -9,10 +9,17 @@
 import UIKit
 import Charts
 
+struct EEntry {
+    var timestamp: Int
+    var temperature: Double
+}
+
 class ChartsVC: UIViewController {
     
 //MARK: Properties
     var dataEntries: [ChartDataEntry] = []
+    var time: [Double] = [9,10,11,12,1,2,3,4,5,6]
+    let prices: [Double] = [24.0, 39, 0.8, -10, -13, -25, -90, 50, 64, 32, 43]
     
 //MARK: IBOutlets
     @IBOutlet weak var chartView: LineChartView!
@@ -20,42 +27,90 @@ class ChartsVC: UIViewController {
 //MARK: AppLifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupLineChart(values: [24.0, 39, 0.8, -10, 50, 64, 32, 43])
+        setupLineChart(values: prices)
+        var entris: [EEntry] = []
+        for i in 0..<prices.count {
+            let entry = EEntry(timestamp: i+9, temperature: prices[i])
+            entris.append(entry)
+        }
+        setChartView(entriesData: entris)
     }
+    
+    func setChartView(entriesData: [EEntry]) {
+        var chartEntries: [ChartDataEntry] = []
+        var xStrings: [String] = []
+        let sortedentriesData = entriesData.sorted { (s1: EEntry, s2: EEntry) -> Bool in //sort by timestamp
+            return s1.timestamp < s2.timestamp
+        }
+        for (i, entry) in sortedentriesData.enumerated() {
+            let newEntry = ChartDataEntry(x: Double(i), y: entry.temperature)
+            chartEntries.append(newEntry) //populate chartEntries - yAxis
+//            let dateFormatter = DateFormatter()
+//            dateFormatter.timeStyle = .medium
+//            dateFormatter.dateStyle = .none
+//            xStrings.append("\(dateFormatter.string(from: Date.init(timeIntervalSince1970: TimeInterval(entry.timestamp))))") //populate xAxis
+            xStrings.append(String(entry.timestamp))
+        }
+        let set: LineChartDataSet = LineChartDataSet(entries: chartEntries, label: "°C")
+        set.setColor(NSUIColor.blue, alpha: CGFloat(1))
+        set.circleColors = [NSUIColor.blue]
+        set.circleRadius = 3
+        set.mode = LineChartDataSet.Mode.linear
+
+        let data: LineChartData = LineChartData(dataSet: set)
+//        self.chartView.xAxis.labelRotationAngle = -90 //tilts label
+        self.chartView.xAxis.valueFormatter = DefaultAxisValueFormatter(block: {(index, _) in
+            return xStrings[Int(index)]
+        })
+        self.chartView.xAxis.setLabelCount(xStrings.count, force: true)
+        self.chartView.xAxis.labelPosition = .bottom //put xAxis labels at the bottom, default at top
+        self.chartView.data = data
+    }
+
     
     fileprivate func setupLineChart(values: [Double]) {
         chartView.noDataText = "No Data available"
         for i in 0..<values.count {
             print("chart point : \(values[i])")
-            let dataEntry = ChartDataEntry(x: Double(i), y: values[i])
-            dataEntries.append(dataEntry)
+//            let dataEntry = ChartDataEntry(x: Double(i), y: values[i])
+            let dataEntryy = ChartDataEntry(x: Double(i), y: values[i], icon: nil, data: nil)
+            dataEntries.append(dataEntryy)
         }
-        let line1 = LineChartDataSet(entries: dataEntries, label: "Units consumed")
-        line1.colors = [.blue]
-        line1.mode = .linear //cubic, horizontal, linear, stepped
-        line1.cubicIntensity = 0.2
+        let lineDataSet = LineChartDataSet(entries: dataEntries, label: "Stock Price")
+        lineDataSet.colors = [.blue]
+        lineDataSet.mode = .linear //cubic, horizontal, linear, stepped
+        lineDataSet.cubicIntensity = 0.2
         
         let gradient = getGradientFilling()
-        line1.fill = Fill.fillWithLinearGradient(gradient, angle: 90.0)
-        line1.drawFilledEnabled = true
-        
+        lineDataSet.fill = Fill.fillWithLinearGradient(gradient, angle: 90.0)
+        lineDataSet.drawFilledEnabled = true
         let data = LineChartData()
-        data.addDataSet(line1)
+        data.addDataSet(lineDataSet)
+        
+//        self.chartView.xAxis.valueFormatter = DefaultAxisValueFormatter(block: {(index, _) in
+//            return String(self.time[Int(index)])
+//        })
+//        self.chartView.xAxis.setLabelCount(time.count, force: true)
+//        self.chartView.xAxis.labelPosition = .bottom //put xAxis labels at the bottom, default at top
+//        self.chartView.data = data
+        
         chartView.data = data
-        chartView.setScaleEnabled(false)
-        chartView.animate(xAxisDuration: 1.5)
-        chartView.drawGridBackgroundEnabled = false
-        chartView.xAxis.drawAxisLineEnabled = false
-        chartView.xAxis.drawGridLinesEnabled = false
-        chartView.leftAxis.drawAxisLineEnabled = false
-        chartView.leftAxis.drawGridLinesEnabled = false
-        chartView.rightAxis.drawAxisLineEnabled = false
-        chartView.rightAxis.drawGridLinesEnabled = false
-        chartView.legend.enabled = false
-        chartView.xAxis.enabled = false
-        chartView.leftAxis.enabled = false
-        chartView.rightAxis.enabled = false
-        chartView.xAxis.drawLabelsEnabled = false
+        
+        
+//        chartView.setScaleEnabled(false)
+//        chartView.animate(xAxisDuration: 1.5)
+//        chartView.drawGridBackgroundEnabled = false
+////        chartView.xAxis.drawAxisLineEnabled = false
+////        chartView.xAxis.drawGridLinesEnabled = false
+//        chartView.leftAxis.drawAxisLineEnabled = false
+//        chartView.leftAxis.drawGridLinesEnabled = false
+//        chartView.rightAxis.drawAxisLineEnabled = false
+//        chartView.rightAxis.drawGridLinesEnabled = false
+//        chartView.legend.enabled = false
+////        chartView.xAxis.enabled = false
+//        chartView.leftAxis.enabled = false
+//        chartView.rightAxis.enabled = false
+////        chartView.xAxis.drawLabelsEnabled = false //draws value of xAxis
         
     }
     
