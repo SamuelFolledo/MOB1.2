229a6dcc11e25ec79bf2a11762086ae669446cee
Samuel Folledo <samuelfolledo@gmail.com>
2020-02-02 18:41:15 -0800

    [add] constant for makeSchool logo image

---
+++ MobileClasswork/MobileClasswork/Helper/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  MobileClasswork
+//
+//  Created by Macbook Pro 15 on 2/2/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+let kMAKESCHOOLIMAGE: UIImage = UIImage(named: "makeschoolLogo")!
