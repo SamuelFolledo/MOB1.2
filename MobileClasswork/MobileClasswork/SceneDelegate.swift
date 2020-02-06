4ccf7002f9e88f5efdd16efb1f28525a7682b122
Samuel Folledo <samuelfolledo@gmail.com>
2020-02-06 09:48:05 -0800

    [add] update scenedelegate to not use storyboard on load

--- MobileClasswork/MobileClasswork/SceneDelegate.swift
+++ MobileClasswork/MobileClasswork/SceneDelegate.swift
@@ -12,12 +12,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: UIScreen.main.bounds) //needed to not use storyboard
+        let vc: Day5_OnboardingVC = Day5_OnboardingVC() //needed to not use storyboard
+        window?.rootViewController = vc //needed to not use storyboard
+        window?.makeKeyAndVisible() //needed to not use storyboard
+        window?.windowScene = windowScene //needed to not use storyboard
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
