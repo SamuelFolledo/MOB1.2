a2167ccc0e2a509fecf3438791b550bcbed1c013
Samuel Folledo <samuelfolledo@gmail.com>
2020-02-06 12:13:38 -0800

    [add] made previous homeVC as the pastBoxesVC, and created anotehr swift file for HomeVC

--- MobileClasswork/MobileClasswork/Onboarding-Project/Controllers/HomeVC.swift
+++ MobileClasswork/MobileClasswork/Onboarding-Project/Controllers/HomeVC.swift
@@ -9,61 +9,22 @@
 import UIKit
 
 class HomeVC: UIViewController {
-//MARK: Properties
-    let alienArray = [String](repeating: "👽 invading  🌎", count: 100)
-    
-//MARK: Properties Views
-    let table: UITableView = {
-       let table = UITableView()
-       table.translatesAutoresizingMaskIntoConstraints = false
-       table.rowHeight = 75
-       return table
-    }()
-    
-//MARK: App Life Cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupViews()
-    }
-    
-//MARK: Private Methods
-    fileprivate func setupViews() {
-        view.backgroundColor = .white
-        setupTableView()
-    }
-    
-    func setupTableView(){
-        table.delegate = self
-        table.dataSource = self
-        self.view.addSubview(table)
-        table.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor).isActive = true
-        table.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor).isActive = true
-        table.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor).isActive = true
-        table.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor).isActive = true
-        table.register(BoxCell.self, forCellReuseIdentifier: "boxCell")
-        table.tableFooterView = UIView()
-    }
-    
-//MARK: Helpers
-    
-}
 
-//MARK: Extensions
-extension HomeVC: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(indexPath.row)
-    }
-}
-
-extension HomeVC: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return alienArray.count
+        // Do any additional setup after loading the view.
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "boxCell", for: indexPath)
-        cell.textLabel?.text = "\(indexPath.row) \(alienArray[indexPath.row])"
-        cell.selectionStyle = .none
-        return cell
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
     }
+    */
+
 }
---
+++ MobileClasswork/MobileClasswork/Onboarding-Project/Controllers/PastBoxesVC.swift
@@ -0,0 +1,69 @@
+//
+//  HomeVC.swift
+//  MobileClasswork
+//
+//  Created by Macbook Pro 15 on 2/6/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+//MARK: Properties
+    let alienArray = [String](repeating: "👽 invading  🌎", count: 100)
+    
+//MARK: Properties Views
+    let table: UITableView = {
+       let table = UITableView()
+       table.translatesAutoresizingMaskIntoConstraints = false
+       table.rowHeight = 75
+       return table
+    }()
+    
+//MARK: App Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+    }
+    
+//MARK: Private Methods
+    fileprivate func setupViews() {
+        view.backgroundColor = .white
+        setupTableView()
+    }
+    
+    func setupTableView(){
+        table.delegate = self
+        table.dataSource = self
+        self.view.addSubview(table)
+        table.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor).isActive = true
+        table.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor).isActive = true
+        table.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor).isActive = true
+        table.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor).isActive = true
+        table.register(BoxCell.self, forCellReuseIdentifier: "boxCell")
+        table.tableFooterView = UIView()
+    }
+    
+//MARK: Helpers
+    
+}
+
+//MARK: Extensions
+extension HomeVC: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
+}
+
+extension HomeVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return alienArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "boxCell", for: indexPath)
+        cell.textLabel?.text = "\(indexPath.row) \(alienArray[indexPath.row])"
+        cell.selectionStyle = .none
+        return cell
+    }
+}
