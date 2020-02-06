36dba12ef853cbc13ce3eee058f8a3c9d7916476
Samuel Folledo <samuelfolledo@gmail.com>
2020-02-06 10:49:03 -0800

    [add] created tableView programmatically

--- MobileClasswork/MobileClasswork/HomeVC.swift
+++ MobileClasswork/MobileClasswork/HomeVC.swift
@@ -10,8 +10,15 @@ import UIKit
 
 class HomeVC: UIViewController {
 //MARK: Properties
+    let alienArray = [String](repeating: "👽 invading  🌎", count: 100)
     
-//MARK: IBOutlets
+//MARK: Properties Views
+    let table: UITableView = {
+       let table = UITableView()
+       table.translatesAutoresizingMaskIntoConstraints = false
+       table.rowHeight = 75
+       return table
+    }()
     
 //MARK: App Life Cycle
     override func viewDidLoad() {
@@ -21,13 +28,38 @@ class HomeVC: UIViewController {
     
 //MARK: Private Methods
     fileprivate func setupViews() {
-        view.backgroundColor = .black
+        view.backgroundColor = .white
+        setupTableView()
     }
     
-//MARK: IBActions
+    func setupTableView(){
+        table.delegate = self
+        table.dataSource = self
+        self.view.addSubview(table)
+        table.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor).isActive = true
+        table.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor).isActive = true
+        table.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor).isActive = true
+        table.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor).isActive = true
+        table.register(BoxCell.self, forCellReuseIdentifier: "boxCell")
+    }
     
 //MARK: Helpers
     
 }
 
 //MARK: Extensions
+extension HomeVC: UITableViewDelegate {
+    
+}
+
+extension HomeVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return alienArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "boxCell", for: indexPath)
+        cell.textLabel?.text = "\(indexPath.row) \(alienArray[indexPath.row])"
+        return cell
+    }
+}
