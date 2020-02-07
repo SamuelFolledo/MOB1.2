8755ee56134ffef11d81f46ca8e29367a2c85e85
Samuel Folledo <samuelfolledo@gmail.com>
2020-02-06 22:43:28 -0800

    [add] page 5 done

--- Projects/TipCalculatorStarter-master/TipCalculatorStarter/ViewController.swift
+++ Projects/TipCalculatorStarter-master/TipCalculatorStarter/ViewController.swift
@@ -9,11 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    // MARK: - View Lifecycle
     
+    @IBOutlet weak var headerView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var themeSwitch: UISwitch!
+    
+    @IBOutlet weak var inputCardView: UIView!
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipPercentSegmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var outputCard: UIView!
+    @IBOutlet weak var tipAmountTitleLabel: UILabel!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var totalAmountTitleLabel: UILabel!
+    @IBOutlet weak var totalAmountLabel: UILabel!
+    
+    
+    
+    
+    
+    
+// MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+//MARK: IBActions
+    @IBAction func resetButtonTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func themeToggled(_ sender: UISwitch) {
+        if sender.isOn {
+            print("switch toggled on")
+        } else {
+            print("switch toggled off")
+        }
+    }
+    
+    
 }
 
