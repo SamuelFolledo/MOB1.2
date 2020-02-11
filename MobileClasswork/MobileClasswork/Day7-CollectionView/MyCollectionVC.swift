4071861119d54126de9934e5773a003b4a309bc9
Samuel Folledo <samuelfolledo@gmail.com>
2020-02-11 10:56:10 -0800

    [add] did classwork activity for collectionView and implemented a subclass of flowlayout

--- MobileClasswork/MobileClasswork/Day7-CollectionView/MyCollectionVC.swift
+++ MobileClasswork/MobileClasswork/Day7-CollectionView/MyCollectionVC.swift
@@ -9,22 +9,136 @@
 import UIKit
 
 class MyCollectionVC: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    let flow = MyFlowLayout()
+    
+    lazy var collectionView: UICollectionView = {
+        
+        // Instantiating the UICollectionView, using the default flow layout
+        let collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        //TODO: Set the datasource & delegate
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        // Customization
+        collectionView.alwaysBounceVertical = true
+        collectionView.backgroundColor = .white
+        
+        //TODO: Register the cell
+        collectionView.register(MyCollectionViewCell.self, forCellWithReuseIdentifier: MyCollectionViewCell.identifier)
+        
+        collectionView.collectionViewLayout = flow
+        return collectionView
+    }()
+    
+    var data: [String] = Array(repeating: "🦕", count: 10)
+    
+    override func loadView() {
+        super.loadView()
+        view.addSubview(collectionView)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Update", style: .done, target: self, action: #selector(showOptions(controller:)))
     }
-    */
-
+    
+    @objc func showOptions(controller: UIViewController) {
+        let alert = UIAlertController(title: "Options", message: "Choose an update", preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "Example", style: .default, handler: { (_) in
+            self.collectionView.performBatchUpdates({ //Animates multiple insert, delete, reload, and move operations as a group.
+                self.data[3] = "😦"
+                let updated = self.data[3]
+                self.data.remove(at: 3)
+                self.data.insert(updated, at: 0)
+                
+                self.collectionView.deleteItems(at: [IndexPath(item: 3, section: 0)])
+                self.collectionView.insertItems(at: [IndexPath(item: 0, section: 0)])
+                
+            }, completion: nil)
+            
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Insert 3 emojis at the beginning", style: .default, handler: { (_) in
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Update item at 5 with an emoji", style: .default, handler: { (_) in
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Delete first 2 items, insert 3 items at the end", style: .default, handler: { (_) in
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Delete first 3, insert 1 item at the beginning", style: .default, handler: { (_) in
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Delete first 3 items, insert 1 item at end", style: .default, handler: { (_) in
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Reset", style: .default, handler: { (_) in
+            self.data = Array(repeating: "🦕", count: 10)
+            self.collectionView.reloadData()
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (_) in
+        }))
+        
+        self.present(alert, animated: true, completion:nil)
+    }
+    
 }
+
+
+extension MyCollectionVC: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        return self.data.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MyCollectionViewCell.identifier, for: indexPath) as! MyCollectionViewCell
+        let data = self.data[indexPath.item]
+        cell.textLabel.text = "\(data) : \(indexPath.row)"
+        cell.textLabel.textColor = UIColor.white
+        return cell
+    }
+}
+
+extension MyCollectionVC: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
+}
+
+//extension MyCollectionVC: UICollectionViewDelegateFlowLayout {
+//
+//    func collectionView(_ collectionView: UICollectionView,
+//                        layout collectionViewLayout: UICollectionViewLayout,
+//                        sizeForItemAt indexPath: IndexPath) -> CGSize { //size of the cells
+//        return CGSize(width: 75, height: 75)
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView,
+//                        layout collectionViewLayout: UICollectionViewLayout,
+//                        insetForSectionAt section: Int) -> UIEdgeInsets { //space between cells and the edge of collectionView
+//        return UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView,
+//                        layout collectionViewLayout: UICollectionViewLayout,
+//                        minimumInteritemSpacingForSectionAt section: Int) -> CGFloat { //if collectionView is vertical, then this is the spacing between cells horizontally
+//        return 20
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView,
+//                        layout collectionViewLayout: UICollectionViewLayout,
+//                        minimumLineSpacingForSectionAt section: Int) -> CGFloat { //spacing between cells vertically
+//        return 20
+//    }
+//
+//}
+
--- MobileClasswork/MobileClasswork/Day7-CollectionView/MyCollectionViewCell.swift
+++ MobileClasswork/MobileClasswork/Day7-CollectionView/MyCollectionViewCell.swift
@@ -10,4 +10,37 @@ import UIKit
 
 class MyCollectionViewCell: UICollectionViewCell {
     
+    static var identifier: String = "Cell"
+    var textLabel: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        // Layout views inside the cell
+        let textLabel = UILabel(frame: .zero)
+        textLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.contentView.addSubview(textLabel)
+        
+        // Aplying constraints
+        NSLayoutConstraint.activate([
+            self.contentView.centerXAnchor.constraint(equalTo: textLabel.centerXAnchor),
+            self.contentView.centerYAnchor.constraint(equalTo: textLabel.centerYAnchor),
+            ])
+        
+        // Customization
+        self.backgroundColor = UIColor.lightGray
+        self.textLabel = textLabel
+        self.textLabel.font = UIFont(name: "Helvetica", size: 26)
+
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+
 }
+
