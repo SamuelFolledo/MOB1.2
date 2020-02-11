7ba96f51d56de6d77948015d5db4a296f4f7a37e
Samuel Folledo <samuelfolledo@gmail.com>
2020-02-11 10:55:02 -0800

    [add] day 7 classwork

---
+++ MobileClasswork/MobileClasswork/Day7-CollectionView/MyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyCollectionViewCell.swift
+//  MobileClasswork
+//
+//  Created by Macbook Pro 15 on 2/11/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ MobileClasswork/MobileClasswork/Onboarding-Project/Model/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  MobileClasswork
+//
+//  Created by Macbook Pro 15 on 2/11/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import Foundation
