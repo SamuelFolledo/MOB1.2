2b7d79f52c124edf633f033d882420111f35efa3
Samuel Folledo <samuelfolledo@gmail.com>
2020-02-12 23:20:16 -0800

    [add] cell and flow layout

---
+++ MobileClasswork/MobileClasswork/Onboarding-Project/View/NewBoxCell.swift
@@ -0,0 +1,13 @@
+//
+//  NewBoxCell.swift
+//  MobileClasswork
+//
+//  Created by Macbook Pro 15 on 2/12/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class NewBoxCell: UICollectionViewCell {
+    
+}
