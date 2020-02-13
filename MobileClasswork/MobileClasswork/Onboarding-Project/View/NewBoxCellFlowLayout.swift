5bb6dcaa9c0e888fcefb4007da2cd22583f5c9f9
Samuel Folledo <samuelfolledo@gmail.com>
2020-02-12 23:20:28 -0800

    [add] populate flow layout

--- MobileClasswork/MobileClasswork/Onboarding-Project/View/NewBoxCellFlowLayout.swift
+++ MobileClasswork/MobileClasswork/Onboarding-Project/View/NewBoxCellFlowLayout.swift
@@ -6,4 +6,20 @@
 //  Copyright © 2020 SamuelFolledo. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class NewBoxCellFlowLayout: UICollectionViewFlowLayout {
+    
+    override func prepare() {
+        super.prepare()
+        guard let cv = collectionView else { return }
+        //self.itemSize = CGSize(width: cv.bounds.inset(by: cv.layoutMargins).size.width, height: 70.0)
+        self.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        self.sectionInsetReference = .fromSafeArea
+        let availableWidth = cv.bounds.inset(by: cv.layoutMargins).size.width
+        let minColumnWidth = CGFloat(300)
+        let maxNumColumns = Int(availableWidth/minColumnWidth)
+        let cellWidth = (availableWidth / CGFloat(maxNumColumns)).rounded(.down)
+        self.itemSize = CGSize(width: cellWidth, height: 200.0)
+    }
+}
