702bffc2db5f80f6b0f7df5042ef26ae9e4d5b5e
Samuel Folledo <samuelfolledo@gmail.com>
2020-02-15 15:03:50 -0800

    [add] some color constants

--- MobileClasswork/MobileClasswork/Helper/Constants.swift
+++ MobileClasswork/MobileClasswork/Helper/Constants.swift
@@ -11,3 +11,8 @@ import UIKit
 let kMAKESCHOOLIMAGE: UIImage = UIImage(named: "makeschoolLogo")!
 let kSTARIMAGE: UIImage = UIImage(named: "star")!
 let kSTARFILLEDIMAGE: UIImage = UIImage(named: "starFilled")!
+
+//color
+let kMAINCOLOR: UIColor = UIColor(hexString: "#6a4bc9", alpha: 1.0)
+let kOFFWHITECOLOR: UIColor = UIColor(hexString: "#f0f0f0")
+let kOFFBLACKCOLOR: UIColor = UIColor(hexString: "#212121")
