9ea616e706a1a5998eb0e6d3ec02992553e236e6
Samuel Folledo <samuelfolledo@gmail.com>
2020-02-15 19:05:52 -0800

    [add] populate day8VC codes

--- MobileClasswork/MobileClasswork/Day8-CompositionalCollection/Day8VC.swift
+++ MobileClasswork/MobileClasswork/Day8-CompositionalCollection/Day8VC.swift
@@ -8,63 +8,72 @@
 
 import UIKit
 
-//class Day8VC: UIViewController  {
-//
-//    var collectionView: UICollectionView!
-//
+class Day8VC: UIViewController  {
+    
+    var collectionView: UICollectionView!
+    
+    lazy var sections: [Section] = [
+        TitleSection(title: "Grid Example"),
+        BasicGridSection(),
+        TitleSection(title: "Featured Categories"),
+        FeaturedSection(),
+        TitleSection(title: "Last Month's Favorites"),
+        FavoritesSection()
+    ]
+    
 //    lazy var sections: [Section] = [
 //        BasicGridSection()
 //    ]
-//
-//    lazy var collectionViewLayout: UICollectionViewLayout = {
-//        var sections = self.sections
-//        let layout = UICollectionViewCompositionalLayout { (sectionIndex, environment) -> NSCollectionLayoutSection? in
-//            return sections[sectionIndex].layoutSection()
-//        }
-//        return layout
-//    }()
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        self.title = "Compositional Layout"
-//        self.view.backgroundColor = UIColor.white
-//        setupCollectionView()
-//    }
-//
-//    func setupCollectionView() {
-//        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: collectionViewLayout)
-//        collectionView.dataSource = self
-//        collectionView.delegate = self
-//        collectionView.backgroundColor = UIColor.white
-//        collectionView.register(UINib(nibName: "TitleCell", bundle: .main), forCellWithReuseIdentifier: TitleCell.identifier)
-//        collectionView.register(UINib(nibName: "FeaturedCell", bundle: .main), forCellWithReuseIdentifier: FeaturedCell.identifier)
-//        collectionView.register(UINib(nibName: "FavoritesCell", bundle: .main), forCellWithReuseIdentifier: FavoritesCell.identifier)
-//        collectionView.register(UINib(nibName: "GridCell", bundle: .main), forCellWithReuseIdentifier: GridCell.identifier)
-//        self.view.addSubview(collectionView)
-//        collectionView.reloadData()
-//    }
-//    override func viewDidAppear(_ animated: Bool) {
-//        super.viewDidAppear(animated)
-//        collectionView.reloadData()
-//    }
-//
-//    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-//        collectionView.reloadData()
-//    }
-//}
+    
+    lazy var collectionViewLayout: UICollectionViewLayout = {
+        var sections = self.sections
+        let layout = UICollectionViewCompositionalLayout { (sectionIndex, environment) -> NSCollectionLayoutSection? in
+            return sections[sectionIndex].layoutSection()
+        }
+        return layout
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Compositional Layout"
+        self.view.backgroundColor = UIColor.white
+        setupCollectionView()
+    }
+    
+    func setupCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: collectionViewLayout)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.backgroundColor = UIColor.white
+        collectionView.register(UINib(nibName: "TitleCell", bundle: .main), forCellWithReuseIdentifier: TitleCell.identifier)
+        collectionView.register(UINib(nibName: "FeaturedCell", bundle: .main), forCellWithReuseIdentifier: FeaturedCell.identifier)
+        collectionView.register(UINib(nibName: "FavoritesCell", bundle: .main), forCellWithReuseIdentifier: FavoritesCell.identifier)
+        collectionView.register(UINib(nibName: "GridCell", bundle: .main), forCellWithReuseIdentifier: GridCell.identifier)
+        self.view.addSubview(collectionView)
+        collectionView.reloadData()
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        collectionView.reloadData()
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        collectionView.reloadData()
+    }
+}
 
-//extension Day8VC: UICollectionViewDataSource {
-//    func numberOfSections(in collectionView: UICollectionView) -> Int {
-//        sections.count
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        sections[section].numberOfItems
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        sections[indexPath.section].configureCell(collectionView: collectionView, indexPath: indexPath)
-//    }
-//}
-//
-//extension Day8VC: UICollectionViewDelegate {}
+extension Day8VC: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        sections.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        sections[section].numberOfItems
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        sections[indexPath.section].configureCell(collectionView: collectionView, indexPath: indexPath)
+    }
+}
+
+extension Day8VC: UICollectionViewDelegate {}
