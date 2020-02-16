04db1e212cfc64fd2c1d4b8f58b37be81370f82e
Samuel Folledo <samuelfolledo@gmail.com>
2020-02-15 19:51:14 -0800

    [add] created instance of all images

--- MobileClasswork/MobileClasswork/Helper/Constants.swift
+++ MobileClasswork/MobileClasswork/Helper/Constants.swift
@@ -8,9 +8,35 @@
 
 import UIKit
 
+//images
 let kMAKESCHOOLIMAGE: UIImage = UIImage(named: "makeschoolLogo")!
 let kSTARIMAGE: UIImage = UIImage(named: "star")!
 let kSTARFILLEDIMAGE: UIImage = UIImage(named: "starFilled")!
+let kMIGRAINEIMAGE: UIImage = UIImage(named: "migraine")!
+let kMEDICATIONIMAGE: UIImage = UIImage(named: "medications")!
+let kOINTMENTSIMAGE: UIImage = UIImage(named: "ointments")!
+let kSPECIALBOXESIMAGE: UIImage = UIImage(named: "specialBoxes")!
+let kSUPPLIESIMAGE: UIImage = UIImage(named: "supplies")!
+
+let kPILLIMAGE: UIImage = UIImage(named: "pill")!
+
+let kCREAMIMAGE: UIImage = UIImage(named: "cream")!
+let kOILIMAGE: UIImage = UIImage(named: "oil")!
+let kBATHIMAGE: UIImage = UIImage(named: "bath")!
+
+let kEYEMASKIMAGE: UIImage = UIImage(named: "eyeMask")!
+let kHEADBANDIMAGE: UIImage = UIImage(named: "headBand")!
+let kEARPLUGIMAGE: UIImage = UIImage(named: "earPlug")!
+let kEYEGLASSESIMAGE: UIImage = UIImage(named: "eyeGlasses")!
+let kHEADMASSAGERIMAGE: UIImage = UIImage(named: "headMassager")!
+let kMOUTHGUARDIMAGE: UIImage = UIImage(named: "mouthGuard")!
+
+let kTEAIMAGE: UIImage = UIImage(named: "tea")!
+let kPREGNANTIMAGE: UIImage = UIImage(named: "pregnant")!
+let kECONOMYBOXIMAGE: UIImage = UIImage(named: "economyBox")!
+let kPREMIUMBOXIMAGE: UIImage = UIImage(named: "premiumBox")!
+let kDELUXEBOXIMAGE: UIImage = UIImage(named: "deluxeBox")!
+let kBOXOFTHEDAYIMAGE: UIImage = UIImage(named: "boxOfTheDay")!
 
 //color
 let kMAINCOLOR: UIColor = UIColor(hexString: "#6a4bc9", alpha: 1.0)
