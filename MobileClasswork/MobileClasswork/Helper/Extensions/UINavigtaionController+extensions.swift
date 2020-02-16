55e05faf743eefbb6afeee8ddb3cd06b77886eb2
Samuel Folledo <samuelfolledo@gmail.com>
2020-02-15 19:06:09 -0800

    [add] a way to customize navigationController's status bar updated for ios 13

--- MobileClasswork/MobileClasswork/Helper/Extensions/UINavigtaionController+extensions.swift
+++ MobileClasswork/MobileClasswork/Helper/Extensions/UINavigtaionController+extensions.swift
@@ -34,4 +34,28 @@ extension UINavigationController {
         transition.type = CATransitionType(rawValue: type)
         self.view.layer.add(transition, forKey: nil)
     }
+    
+///Change the background color of the status bar and if isLightContent
+    func setStatusBarColor(backgroundColor: UIColor, isLightContent: Bool = true) {
+        if #available(iOS 13, *) {
+            let navBarAppearance = UINavigationBarAppearance()
+            navBarAppearance.configureWithOpaqueBackground()
+//            navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+            let titleAttributes = [
+                NSAttributedString.Key.foregroundColor: kOFFWHITECOLOR,
+//            NSAttributedString.Key.font: UIFont(name: "Georgia-Bold", size: 24)!
+            ]
+            navBarAppearance.largeTitleTextAttributes = titleAttributes
+            navBarAppearance.backgroundColor = backgroundColor
+            self.navigationBar.standardAppearance = navBarAppearance
+            self.navigationBar.scrollEdgeAppearance = navBarAppearance
+        } else {
+            // ADD THE STATUS BAR AND SET A CUSTOM COLOR
+            let statusBar: UIView = UIApplication.shared.value(forKey: "statusBar") as! UIView
+            if statusBar.responds(to:#selector(setter: UIView.backgroundColor)) {
+                statusBar.backgroundColor = backgroundColor
+            }
+            UIApplication.shared.statusBarStyle = isLightContent ? .lightContent : .darkContent
+        }
+    }
 }
