1fcbc366b5be26a6be74e347360703cba5c05fbb
Samuel Folledo <samuelfolledo@gmail.com>
2020-02-16 22:16:37 -0800

    [add] sample products

--- MobileClasswork/MobileClasswork/Helper/Constants.swift
+++ MobileClasswork/MobileClasswork/Helper/Constants.swift
@@ -8,6 +8,26 @@
 
 import UIKit
 
+let ibuprofen = Product(name: "Ibuprofen", description: "OTC Pills", image: kPILLIMAGE)
+let aspirin = Product(name: "Aspirin", description: "OTC Pills", image: kPILLIMAGE)
+let excedrin = Product(name: "Excedrin", description: "OTC Pills", image: kPILLIMAGE)
+let acetaminophen = Product(name: "Acetaminophen", description: "OTC Pills", image: kPILLIMAGE)
+let propanolol = Product(name: "Propanolol", description: "RX Pills", image: kPILLIMAGE)
+let migramax = Product(name: "Migramax", description: "RX Pills", image: kPILLIMAGE)
+let toradol = Product(name: "Toradol", description: "RX Pills", image: kPILLIMAGE)
+let headBand = Product(name: "Head Band", description: "Migraine Gadgets", image: kHEADBANDIMAGE)
+let mouthGuard = Product(name: "Toradol", description: "Migraine Gadgets", image: kMOUTHGUARDIMAGE)
+let headMassager = Product(name: "Head Massager", description: "Migraine Gadgets", image: kHEADMASSAGERIMAGE)
+let eyeGlasses = Product(name: "Light Sensitive Glasses", description: "Migraine Gadgets", image: kEYEGLASSESIMAGE)
+let oil1 = Product(name: "Lavander Oil", description: "Oils", image: kOILIMAGE)
+let oil2 = Product(name: "Peppermint Oil", description: "Oils", image: kOILIMAGE)
+let tea1 = Product(name: "Chamomile Tea", description: "Tea", image: kTEAIMAGE)
+let tea2 = Product(name: "Peppermint Tea", description: "Tea", image: kTEAIMAGE)
+let pregnant1 = Product(name: "Back Brace", description: "Pregnant Support", image: kPREGNANTIMAGE)
+let pregnant2 = Product(name: "Pillow", description: "Pregnant Support", image: kPREGNANTIMAGE)
+let kSAMPLEPRODUCT: Product = Product(name: "Advil", description: "OTC Pills", image: kPILLIMAGE)
+let kSAMPLEPRODUCTS: [Product] = [acetaminophen, ibuprofen, excedrin, aspirin, propanolol, migramax, toradol, headBand, mouthGuard, headMassager, eyeGlasses, oil1, oil2, tea1, tea2, pregnant1, pregnant2]
+
 //images
 let kMAKESCHOOLIMAGE: UIImage = UIImage(named: "makeschoolLogo")!
 let kSTARIMAGE: UIImage = UIImage(named: "star")!
