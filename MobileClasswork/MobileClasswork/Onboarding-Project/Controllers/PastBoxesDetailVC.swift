9bdb650142a9cb7987cdc11740f6fc707766040e
Samuel Folledo <samuelfolledo@gmail.com>
2020-02-16 22:19:16 -0800

    [add] implemented cell and populate with sample products

--- MobileClasswork/MobileClasswork/Onboarding-Project/Controllers/PastBoxesDetailVC.swift
+++ MobileClasswork/MobileClasswork/Onboarding-Project/Controllers/PastBoxesDetailVC.swift
@@ -10,8 +10,7 @@ import UIKit
 
 class PastBoxesDetailVC: UIViewController {
 //MARK: Properties
-    var boxes = [(Int, Bool)](repeating: (1,false), count: 100)
-//    let boxes = []
+    var boxes: [(Product, Bool)] = [(Product, Bool)](repeating: (kSAMPLEPRODUCT,false), count: 100)
     var month: String = ""
     
 //MARK: Properties Views
@@ -53,12 +52,11 @@ class PastBoxesDetailVC: UIViewController {
         table.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor).isActive = true
         table.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor).isActive = true
         table.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor).isActive = true
-        table.register(BoxDetailCell.self, forCellReuseIdentifier: "boxDetailCell")
+        table.register(BoxDetailCell.self, forCellReuseIdentifier: BoxDetailCell.identifier)
         table.tableFooterView = UIView()
     }
     
 //MARK: Helpers
-    
 }
 
 //MARK: Extensions
@@ -76,7 +74,7 @@ extension PastBoxesDetailVC: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell: BoxDetailCell = tableView.dequeueReusableCell(withIdentifier: "boxDetailCell", for: indexPath) as! BoxDetailCell
+        let cell: BoxDetailCell = tableView.dequeueReusableCell(withIdentifier: BoxDetailCell.identifier, for: indexPath) as! BoxDetailCell
         //cell.starImageView.image = cell.isFavorite ? kSTARFILLEDIMAGE : kSTARIMAGE
         cell.selectionStyle = .none
         if boxes[indexPath.row].1 { //if true
@@ -84,9 +82,13 @@ extension PastBoxesDetailVC: UITableViewDataSource {
         } else {
             cell.starImageView.image = kSTARIMAGE
         }
+        let product = boxes[indexPath.row].0
+        cell.boxImageView.image = product.image.tint(with: SettingsService.grayColor) //product's image
         cell.backgroundColor = SettingsService.whiteColor
-        cell.titleLabel.textColor = SettingsService.darkGrayColor
-        cell.descriptionLabel.textColor = SettingsService.darkGrayColor
+        cell.titleLabel.textColor = SettingsService.grayColor
+        cell.titleLabel.text = product.name
+        cell.descriptionLabel.textColor = SettingsService.grayColor
+        cell.descriptionLabel.text = product.description
         return cell
     }
 }
