62761536d148c0224cd5c4962d67b1f1eeda9a9c
Samuel Folledo <samuelfolledo@gmail.com>
2020-02-19 22:45:05 -0800

    [add] flip animationVC

---
+++ MobileClasswork/MobileClasswork/Playground/FlipAnimationVC.swift
@@ -0,0 +1,30 @@
+//
+//  FlipAnimationVC.swift
+//  MobileClasswork
+//
+//  Created by Macbook Pro 15 on 2/19/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class FlipAnimationVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+//    @objc func flipStockViewTap(_ gesture: UITapGestureRecognizer) { //dismiss fields
+//        isDescriptionView = !isDescriptionView
+//        let transitionOptions: UIView.AnimationOptions = [.transitionFlipFromRight, .showHideTransitionViews]
+//
+//        UIView.transition(with: graphView, duration: 0.5, options: transitionOptions, animations: {
+//            self.graphView.isHidden = self.isDescriptionView
+//        })
+//
+//        UIView.transition(with: descriptionView, duration: 0.5, options: transitionOptions, animations: {
+//            self.descriptionView.isHidden = !self.isDescriptionView
+//        })
+//    }
+}
