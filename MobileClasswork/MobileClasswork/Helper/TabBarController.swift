591c30cc87b34b0b053d50e7f453618373223c3c
Samuel Folledo <samuelfolledo@gmail.com>
2020-02-22 11:02:47 -0800

    [add] fixed tabbar item sizing

--- MobileClasswork/MobileClasswork/Helper/TabBarController.swift
+++ MobileClasswork/MobileClasswork/Helper/TabBarController.swift
@@ -9,11 +9,12 @@
 import UIKit
 
 class TabBarController: UITabBarController {
-    ///bounce animation on tabbar item
+    
+///tabbar item bounce animation
     private var bounceAnimation: CAKeyframeAnimation = {
         let bounceAnimation = CAKeyframeAnimation(keyPath: "transform.scale")
         bounceAnimation.values = [1.0, 1.4, 0.9, 1.02, 1.0]
-        bounceAnimation.duration = TimeInterval(0.3)
+        bounceAnimation.duration = TimeInterval(0.5)
         bounceAnimation.calculationMode = CAAnimationCalculationMode.cubic
         return bounceAnimation
     }()
@@ -25,31 +26,24 @@ class TabBarController: UITabBarController {
     
     fileprivate func setupTabBar() {
         self.delegate = self
+        self.tabBar.isTranslucent = false
         setupViewControllers()
     }
     
     fileprivate func setupViewControllers(){
         let homeNav = UINavigationController(rootViewController: HomeVC())
-//        homeNav.tabBarItem
+        homeNav.tabBarItem = UITabBarItem(title: "Home", image: kHOMEIMAGE, tag: 0)
         let newNav = UINavigationController(rootViewController: NewBoxVC())
+        newNav.tabBarItem = UITabBarItem(title: "Home", image: kNEWIMAGE, tag: 1)
         let historyNav = UINavigationController(rootViewController: PastBoxesVC())
+        historyNav.tabBarItem = UITabBarItem(title: "History", image: kHISTORYIMAGE, tag: 2)
         let profileNav = UINavigationController(rootViewController: ProfileVC())
         let navArray: [UINavigationController] = [ homeNav, newNav, historyNav, profileNav]
+        profileNav.tabBarItem = UITabBarItem(title: "Profile", image: kPROFILEIMAGE, tag: 3)
         viewControllers = navArray
     }
     
-    /// didSelect that can trigger animation
-//    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
-//        // find index if the selected tab bar item, then find the corresponding view and get its image, the view position is offset by 1 because the first item is the background (at least in this case)
-//        guard let idx = tabBar.items?.firstIndex(of: item), tabBar.subviews.count > idx + 1, let imageView = tabBar.subviews[idx + 1].subviews.compactMap ({ $0 as? UIImageView }).first else {
-//            return
-//        }
-//        // animate the imageView
-//        imageView.layer.add(bounceAnimation, forKey: nil)
-//    }
-    
 //MARK: Helpers
-    
 }
 
 extension TabBarController: UITabBarControllerDelegate {
@@ -65,7 +59,7 @@ extension TabBarController: UITabBarControllerDelegate {
         return TransitioningObject(tabBarController: tabBarController, fromIndex: fromIndex)
     }
     
-/// didSelect that can bar item trigger animation
+/// tabbar item bounce animation
     override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
         // find index if the selected tab bar item, then find the corresponding view and get its image, the view position is offset by 1 because the first item is the background (at least in this case)
         guard let idx = tabBar.items?.firstIndex(of: item), tabBar.subviews.count > idx + 1, let imageView = tabBar.subviews[idx + 1].subviews.compactMap ({ $0 as? UIImageView }).first else {
