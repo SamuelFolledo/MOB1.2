093b01adf7f44326d957b5974a739da6565c5cc4
Samuel Folledo <samuelfolledo@gmail.com>
2020-02-22 14:05:07 -0800

    [add] populated views in profileVC

--- MobileClasswork/MobileClasswork/Onboarding-Project/Controllers/ProfileVC.swift
+++ MobileClasswork/MobileClasswork/Onboarding-Project/Controllers/ProfileVC.swift
@@ -10,21 +10,49 @@ import UIKit
 
 class ProfileVC: UIViewController {
 //MARK: Properties
+    
+//MARK: Properties Views
+    lazy var stackView: UIStackView = {
+        let stackView: UIStackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .fillEqually
+        stackView.spacing = 20
+        return stackView
+    }()
+    lazy var newBoxButton: UIButton = {
+        let button: UIButton = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("New Box", for: .normal)
+        return button
+    }()
+    lazy var pastBoxesButton: UIButton = {
+        let button: UIButton = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Past Boxes", for: .normal)
+        return button
+    }()
+    lazy var profileButton: UIButton = {
+        let button: UIButton = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Profile", for: .normal)
+        return button
+    }()
     lazy var settingsButton: UIBarButtonItem = {
         let barButton: UIBarButtonItem = UIBarButtonItem()
         return barButton
     }()
     
 //MARK: App Life Cycle
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setupViews()
-    }
-    
     override func loadView() {
         super.loadView()
         title = "Profile"
-        setupNavigationBar()
+        setupViews()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -33,16 +61,52 @@ class ProfileVC: UIViewController {
         updateColors()
     }
     
-//MARK: Navigation
-    
-//MARK: Private Methods
-    fileprivate func setupViews() {
-        
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        updateColors()
     }
     
+//MARK: Private Methods
     fileprivate func updateColors() {
         view.backgroundColor = SettingsService.whiteColor
+        newBoxButton.isMainButton()
+        pastBoxesButton.isMainButton()
+        profileButton.isMainButton()
         self.tabBarController?.isMainTabBar()
+    }
+    
+    fileprivate func setupViews() {
+        self.view.addSubview(stackView)
+        NSLayoutConstraint.activate([
+            stackView.widthAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.8),
+            stackView.heightAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.8),
+            stackView.centerYAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerYAnchor),
+            stackView.centerXAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerXAnchor)
+        ])
+        setupStackView()
+        setupButtons()
+    }
+    
+    fileprivate func setupStackView() {
+        self.view.addSubview(stackView)
+        NSLayoutConstraint.activate([
+            stackView.widthAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.8),
+            stackView.heightAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.8),
+            stackView.centerYAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerYAnchor),
+            stackView.centerXAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerXAnchor)
+        ])
+    }
+    
+    fileprivate func setupButtons() {
+        stackView.addArrangedSubview(newBoxButton)
+        newBoxButton.widthAnchor.constraint(equalTo: stackView.widthAnchor, multiplier:  0.8).isActive = true
+        newBoxButton.addTarget(self, action: #selector(newButtonTapped), for: .touchUpInside)
+        stackView.addArrangedSubview(pastBoxesButton)
+        pastBoxesButton.widthAnchor.constraint(equalTo: newBoxButton.widthAnchor).isActive = true
+        pastBoxesButton.addTarget(self, action: #selector(pastButtonTapped), for: .touchUpInside)
+        stackView.addArrangedSubview(profileButton)
+        profileButton.widthAnchor.constraint(equalTo: newBoxButton.widthAnchor).isActive = true
+        profileButton.addTarget(self, action: #selector(profileButtonTapped), for: .touchUpInside)
     }
     
     fileprivate func setupNavigationBar() {
@@ -54,7 +118,23 @@ class ProfileVC: UIViewController {
         navigationItem.rightBarButtonItem = settingsButton
     }
     
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
 //MARK: Helpers
+    @objc func newButtonTapped() {
+        let vc: NewBoxVC = NewBoxVC()
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    @objc func pastButtonTapped() {
+        let vc: PastBoxesVC = PastBoxesVC()
+        navigationController?.pushViewController(vc, animated: true) //push
+    }
+    @objc func profileButtonTapped() {
+        let vc: Day8VC = Day8VC()
+        navigationController?.pushViewController(vc, animated: true) //push
+    }
     @objc func settingsButtonTapped() {
         let vc: PopupVC = PopupVC()
         navigationController?.pushViewController(vc, animated: false) //push
@@ -62,4 +142,3 @@ class ProfileVC: UIViewController {
 }
 
 //MARK: Extensions
-
