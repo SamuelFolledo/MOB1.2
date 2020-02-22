bdfc6a8e8977962bb4cb1038ff3de8447b8fa25b
Samuel Folledo <samuelfolledo@gmail.com>
2020-02-21 21:48:58 -0800

    [add] missing files

---
+++ MobileClasswork/MobileClasswork/Onboarding-Project/Controllers/ProfileVC.swift
@@ -0,0 +1,37 @@
+//
+//  ProfileVC.swift
+//  MobileClasswork
+//
+//  Created by Macbook Pro 15 on 2/21/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class ProfileVC: UIViewController {
+//MARK: Properties
+    
+//MARK: IBOutlets
+    
+//MARK: App Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+    }
+    
+//MARK: Navigation
+    
+//MARK: Private Methods
+    fileprivate func setupViews() {
+        self.title = "My Profile"
+//        self.navigationController!.navigationBar.isTranslucent = false
+    }
+    
+//MARK: IBActions
+    
+//MARK: Helpers
+    
+}
+
+//MARK: Extensions
+
