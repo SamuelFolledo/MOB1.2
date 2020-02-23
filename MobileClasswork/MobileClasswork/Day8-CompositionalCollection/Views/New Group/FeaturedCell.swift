c9c366ba5dab5c79539a686f96e5395ef15fcac6
Samuel Folledo <samuelfolledo@gmail.com>
2020-02-22 16:57:16 -0800

    [add] image for customization

--- MobileClasswork/MobileClasswork/Day8-CompositionalCollection/Views/New Group/FeaturedCell.swift
+++ MobileClasswork/MobileClasswork/Day8-CompositionalCollection/Views/New Group/FeaturedCell.swift
@@ -9,9 +9,20 @@
 import UIKit
 
 class FeaturedCell: UICollectionViewCell {
+    var image: UIImage = kMAKESCHOOLIMAGE {
+        didSet {
+            imgView.image = image
+        }
+    }
     static var identifier: String = "FeaturedCell"
     @IBOutlet weak var imgView: UIImageView!
-
+    
+//    required init(image: UIImage) { //allows to populate views from parameter
+//        self.image = image
+//        super.init(frame: .zero)
+//        setup()
+//    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         setup()
@@ -20,5 +31,6 @@ class FeaturedCell: UICollectionViewCell {
     func setup(){
         imgView.backgroundColor = .red
         imgView.layer.cornerRadius = 10
+        imgView.image = image
     }
 }
